1af4a45deb32cf2362fd5122a14108af92cd6ebd
EdmondKirsc <arzhanovpavel@mail.ru>
2021-03-08 20:22:32 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+  let str = String(n); 
+  let arr = str.split('');
+  let reversedArr = arr.reverse(); 
+  let reversedStr = reversedArr.join(''); 
+  let reversedNumber = Number(reversedStr);  
+  return reversedNumber;
 }
